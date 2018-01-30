5f6934bff5b81177a5c2ff5b6c54ff2d9ca25f3c
JP Leigh <jpleigh@JPs-MacBook-Pro.local>
2018-01-30 11:43:30 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
 
 
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+{
+	card: 'queen',
+	suit: 'hearts',
+	cardImage: 'images/queen-of-hearts.png'
+},
+{
+	card: 'queen',
+	suit: 'diamonds',
+	cardImage: 'images/queen-of-diamonds.png'
+},
+{
+	card: 'king',
+	suit: 'hearts',
+	cardImage: 'images/king-of-hearts.png'
+},
+{
+	card: 'king',
+	suit: 'diamonds',
+	cardImage: 'images/king-of-diamonds.png'
+},
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function () {
@@ -13,8 +35,12 @@ if (cardsInPlay[0] === cardsInPlay[1]) {
 
 
 var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].card);
+	cardsInPlay.push(cards[cardId].card);
+
+	console.log(cards[cardId].cardImage)
+	console.log(cards[cardId].suit)
+
 
 if (cardsInPlay.length === 2) {
 	checkForMatch();
